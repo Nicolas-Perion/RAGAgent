8e019b41b2f3c80c71cf265f1e97f7b7edc6e2dc
Nicolas Perion <nicolas.perion.quemeneur@gmail.com>
2026-06-16 18:41:20 +0200

    fix: problems regarding the EC2 instance + changes in requirements.txt

--- vectorstore.py
+++ vectorstore.py
@@ -9,7 +9,7 @@ S3_BUCKET = os.getenv("S3_BUCKET")
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
 if not os.path.exists(VECTORSTORE_PATH):
-    s3 = boto3.client("s3")
+    s3 = boto3.client("s3", region_name="eu-north-1")
     s3.download_file(S3_BUCKET, "vectorstore.json", VECTORSTORE_PATH)
 
 vectorstore = InMemoryVectorStore.load(VECTORSTORE_PATH, embeddings)
